7f58bb4362c92c8f55d05615f9d78a290016bf1f
shlomi amar <shlomia1074@gmail.com>
2022-03-14 11:02:32 +0200

    update yolo config to be relative

--- Code/Models/YOLOV3/core/config.py
+++ Code/Models/YOLOV3/core/config.py
@@ -23,8 +23,8 @@ cfg                           = __C
 __C.YOLO                      = edict()
 
 # Set the class name
-__C.YOLO.CLASSES              = r"E:\FinalProject\code\Models\YOLOV3\data\classes\classes.names"
-__C.YOLO.ANCHORS              = r"E:\FinalProject\code\Models\YOLOV3\data\anchors\basline_anchors.txt"
+__C.YOLO.CLASSES              = r"../Code/Models/YOLOV3/data/classes/classes.names"
+__C.YOLO.ANCHORS              = r"../Code/Models/YOLOV3/data\anchors\basline_anchors.txt"
 __C.YOLO.STRIDES              = [8, 16, 32]
 __C.YOLO.ANCHOR_PER_SCALE     = 3
 __C.YOLO.IOU_LOSS_THRESH      = 0.5
@@ -32,7 +32,7 @@ __C.YOLO.IOU_LOSS_THRESH      = 0.5
 # Train options
 __C.TRAIN                     = edict()
 
-__C.TRAIN.ANNOT_PATH          = r"E:\FinalProject\code\Models\YOLOV3\data\dataset\annotation.txt"
+__C.TRAIN.ANNOT_PATH          = r"../Code/Models\YOLOV3\data\dataset\annotation.txt"
 __C.TRAIN.BATCH_SIZE          = 8
 # __C.TRAIN.INPUT_SIZE            = [320, 352, 384, 416, 448, 480, 512, 544, 576, 608]
 __C.TRAIN.INPUT_SIZE          = [416]
@@ -47,11 +47,11 @@ __C.TRAIN.LOAD_WEIGHTS        = False
 # TEST options
 __C.TEST                      = edict()
 
-__C.TEST.ANNOT_PATH           = r"E:\FinalProject\Models\YOLOV3\data\dataset\annotation_test.txt"
+__C.TEST.ANNOT_PATH           = r"../Code/Models/YOLOV3/data\dataset\annotation_test.txt"
 __C.TEST.BATCH_SIZE           = 2
 __C.TEST.INPUT_SIZE           = 416
 __C.TEST.DATA_AUG             = False
-__C.TEST.DECTECTED_IMAGE_PATH = "E:\FinalProject\code\Models\OLOV3\data\detection\\tanks_airships\\"
+__C.TEST.DECTECTED_IMAGE_PATH = "../Code/Models/YOLOV3/data\detection\\tanks_airships\\"
 __C.TEST.SCORE_THRESHOLD      = 0.5
 __C.TEST.IOU_THRESHOLD        = 0.5
 
